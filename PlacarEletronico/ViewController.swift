663d2c87e12bc5a0cf1dc36e0d6d516347dc147a
Ricardo Varjão <rvarjao@gmail.com>
2019-10-28 23:18:39 -0300

    Initial Commit

---
+++ PlacarEletronico/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacarEletronico
+//
+//  Created by Ricardo Varjão on 28/10/19.
+//  Copyright © 2019 Ricardo Varjão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
